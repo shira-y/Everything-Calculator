ce006826578b0d19e7e2645f370485ce06c0eca8
[dyad] <syaish@g.jct.ac.il>
2025-11-13 00:06:13 +0200

    [dyad] Fixed TypeScript type errors in toast utility - wrote 1 file(s)

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -4,11 +4,11 @@ export const showSuccess = (message: string) => {
   toast.success(message);
 };
 
-export const showError = (message: string) => {
+export const const showError = (message: string) => {
   toast.error(message);
 };
 
-export const showLoading = (message: string) => {
+export const showLoading = (message: string): string => {
   return toast.loading(message);
 };
 
